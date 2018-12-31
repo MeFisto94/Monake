f99a8e97084b235fee4a96b8b8894de4c2d7a931
MeFisto94 <MeFisto94@users.noreply.github.com>
2018-12-31 18:04:39 +0100

    Load the Collision Shape for the Map if it works.

--- src/main/java/com/jmonkeyengine/monake/sim/CollisionShapeProvider.java
+++ src/main/java/com/jmonkeyengine/monake/sim/CollisionShapeProvider.java
@@ -1,12 +1,17 @@
 package com.jmonkeyengine.monake.sim;
 
+import com.jme3.asset.AssetNotFoundException;
 import com.jme3.bullet.collision.shapes.BoxCollisionShape;
 import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
 import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.bullet.util.CollisionShapeFactory;
 import com.jme3.math.Vector3f;
+import com.jme3.scene.Spatial;
 import com.jmonkeyengine.monake.bullet.CollisionShapes;
 import com.jmonkeyengine.monake.bullet.ShapeInfo;
 import com.jmonkeyengine.monake.es.ShapeInfos;
+import com.jmonkeyengine.monake.util.server.ServerApplication;
 import com.simsilica.es.EntityData;
 
 /**
@@ -31,7 +36,13 @@ public class CollisionShapeProvider {
 
         // @TODO: Is there a better way?
         if (shapeInfo.getShapeId() == ShapeInfos.worldInfo(entityData).getShapeId()) {
-            return new BoxCollisionShape(new Vector3f(64f, 0.5f, 64f)); // See ModelViewState
+            // Taken from RigidBodyControl code :D
+            try {
+                Spatial spatial = ServerApplication.self.getAssetManager().loadModel("Models/level.j3o");
+                return CollisionShapeFactory.createMeshShape(spatial);
+            } catch (AssetNotFoundException anf) {
+                return new BoxCollisionShape(new Vector3f(64f, 0.5f, 64f)); // See ModelViewState
+            }
         } else if (shapeInfo.getShapeId() == ShapeInfos.boxInfo(entityData).getShapeId()) {
             return new BoxCollisionShape(new Vector3f(1f, 1f, 1f));
         } else if (shapeInfo.getShapeId() == ShapeInfos.playerInfo(entityData).getShapeId()) {
